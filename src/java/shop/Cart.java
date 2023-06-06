1932752c5ba6b1d610fef087f9ec16ab170ec409
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-06-06 17:13:13 +0200

    cart modified Object[] in Product[] & Headphone extends Product

--- src/java/shop/Cart.java
+++ src/java/shop/Cart.java
@@ -1,20 +1,19 @@
 package java.shop;
 
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Cart {
   public static void main(String[] args) {
     System.out.println("Benvenuto in negozio");
     Scanner scanner = new Scanner(System.in);
-    
+
 //  inserisci il numero di prodotti che vuoi acquistare
 //  fill i campi del prodotto con i dati del constructor
 //  chiedi all' utente di specificare la classe di prodotto che vuole acquistare'(smartphone tv hf)
 //  al termine dell' inserimento stampa il riepilogo carrello (metodo toString)
-  System.out.println("Quanti prodotti vuoi acquistare?");
-  int nProdotti = scanner.nextInt();
-    Object[] cart = new Object[nProdotti];
+    System.out.println("Quanti prodotti vuoi acquistare?");
+    int nProdotti = scanner.nextInt();
+    Product[] cart = new Product[nProdotti];
     for (int i = 0; i < nProdotti; i++) {
       System.out.println("Inserisci la tipologia di prodotto");
       System.out.println("1 per Smartphone");
@@ -24,28 +23,51 @@ public class Cart {
       int scelta;
       System.out.print("Scelta: ");
       scelta = scanner.nextInt();
+  
+      System.out.println("Inserisci il nome del prodotto");
+      String nome = scanner.nextLine();
       
-      switch (scelta) {
-        case 1 -> {
-        Smartphone smartphone = new Smartphone();
-        System.out.println("Inserisci il nome del prodotto");
-        
-        }
-        case 2 -> {
-        
-        }
-        case 3 ->  {
-        
-        }
-        case 4 -> System.out.println("Grazie per aver utilizzato il nostro negozio. Arrivederci!");
-        
-        default -> System.out.println("Scelta non valida. Riprova.");
+      System.out.println("Inserisci la descrizione del prodotto");
+      String descrizione = scanner.nextLine();
       
-      String nome = scanner.next();
       System.out.println("Inserisci il prezzo del prodotto");
       double prezzo = scanner.nextDouble();
-      cart[i] = new Prodotto(nome, prezzo);
-    }
-  }
+      
+      System.out.println("Inserisci il iva del prodotto");
+      int iva = scanner.nextInt();
   
+      switch (scelta) {
+        case 1 -> {
+          
+          System.out.println("Inserisci l' IMEI del prodotto");
+          String IMEI = scanner.nextLine();
+          
+          System.out.println("Inserisci la memoria del prodotto");
+          int memory = scanner.nextInt();
+          
+          cart[i] = new Smartphone(nome,descrizione,prezzo,iva,IMEI,memory);
+        }
+        case 2 -> {
+          System.out.println("Inserisci la dimensione in pollici del prodotto");
+          int dimension = scanner.nextInt();
+          
+          System.out.println("Inserisci la memoria del prodotto");
+          boolean isSmart = scanner.nextBoolean();
+          
+          cart[i] = new TV(nome,descrizione,prezzo,iva,dimension,isSmart);
+        }
+        case 3 -> {
+          System.out.println("Inserisci la dimensione in pollici del prodotto");
+          String color = scanner.nextLine();
+          
+          System.out.println("Inserisci la memoria del prodotto");
+          boolean isCabled = scanner.nextBoolean();
+          cart[i] = new Headphone(nome,descrizione,prezzo,iva,color,isCabled);
+        }
+        
+        default -> System.out.println("Scelta non valida. Riprova.");
+      }
+    }
+    
+  }
 }
--- src/java/shop/Headphone.java
+++ src/java/shop/Headphone.java
@@ -1,8 +1,14 @@
 package java.shop;
 
-public class Headphone {
+public class Headphone extends Product {
   private String color;
   private boolean isCabled;
+  
+  public Headphone(String nome, String descrizione, double prezzo, int iva, String color, boolean isCabled) {
+    super(nome, descrizione, prezzo, iva);
+    this.color = color;
+    this.isCabled = isCabled;
+  }
   
   public String getColor() {
     return color;
